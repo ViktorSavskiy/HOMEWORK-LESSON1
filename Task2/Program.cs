f0d53609cd4b7b83f697acdf487d310c1ebd664e
Viktor <savskij@bk.ru>
2022-08-20 15:00:50 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input first number");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number");
+int b=int.Parse(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Max number first, Min number second");
+} 
+else
+{
+    Console.WriteLine("Max number second, Min number first");
+}
