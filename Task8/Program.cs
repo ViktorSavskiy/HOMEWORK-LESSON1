61199ba0772093aa1bc898d36f9e82c5e6dfe300
Viktor <savskij@bk.ru>
2022-08-20 23:11:53 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите положительное число");
+int num=int.Parse(Console.ReadLine());
+int a = 1;
+    if (num<0)
+    {
+    Console.WriteLine("Число должно быть больше ноля");
+}
+ if (num==0)
+    {
+    Console.WriteLine("Больше ноля");
+}
+while (a<=num)
+{
+    if (a%2==0)
+    {
+    Console.WriteLine(a);
+}
+a = a + 1; 
+}
