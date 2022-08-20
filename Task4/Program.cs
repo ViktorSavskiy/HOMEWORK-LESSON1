cf0df64d426a26e0a9c8b6c773335b8f7b833304
Viktor <savskij@bk.ru>
2022-08-20 22:28:48 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Input first number");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number");
+int b=int.Parse(Console.ReadLine());
+Console.WriteLine("Input third number");
+int c=int.Parse(Console.ReadLine());
+Console.WriteLine("Max number:");
+if (a>b && a>c)
+{
+    Console.WriteLine(a);
+} 
+if (a<b && b>c)
+{
+    Console.WriteLine(b);
+} 
+else
+{
+    Console.WriteLine(c);
+}
+
